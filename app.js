407d7f4403d922b41fb59a6fcc3db2661b7e52d8
TimWang95 <tgm761@gmail.com>
2023-01-20 11:25:04 +0800

    restaurant list

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/search', (req, res) => {
+  // console.log('req.query', req.query)
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  // console.log('req.params.restaurant_id:', req.params.restaurant_id)
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+
+
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`)
+})
